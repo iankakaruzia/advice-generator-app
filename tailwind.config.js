8407b746d18da892370d91b95bbb28e10810abdc
Ianka Karuzia <irabelo@questrade.com>
2022-08-04 14:54:35 -0300

    chore: add design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Manrope", "sans-serif"],
+      },
+      boxShadow: {
+        lg: 'box-shadow: 0px 0px 40px #53FFAA',
+        xl: '30px 50px 80px rgba(0, 0, 0, 0.100202)'
+      },
+      borderRadius: {
+        xl: '0.625rem'
+      },
+      colors: {
+        green: {
+          100: "#CEE3E9",
+          500: "#53FFAA",
+        },
+        gray: {
+          500: "#313A48",
+          700: "#202733"
+        }
       }
     },
   },
